8781f21b36bd7f741f0789a3def598605b6f0fa5
Nikita Volkov <volkov.nikita@phystech.edu>
2021-12-05 17:13:10 +0300

    create blood

--- blood/scripts/bloodProcessing.py
+++ blood/scripts/bloodProcessing.py
@@ -24,52 +24,52 @@ p_ADC_to_Hg = np.polyfit(pressureADC, pressureHg, 1)
 print(p_ADC_to_Hg)
 
 # начало построение калибровочного графика
-# w = 8
-# h = 5
-# dpi = 400
-# fig, ax = plt.subplots(figsize=(w,h), dpi=dpi)
-#
-# ax.scatter(pressureHg, pressureADC,
-#            label="Измерения", c="blue", s=40, marker='*', alpha=1)
-#
-# ax.plot(pressureHg, pressureADC,
-#         linestyle='-', linewidth=1.5, color='orange',
-#         label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
-#         )
-#
-# ax.legend(loc="upper left") # легенда
-#
-# # макс и мин занчения по осям
-# ax_x_min = min(pressureHg) - 5/100 * (max(pressureHg) - min(pressureHg)) # на 5% длины всего графика вниз от минимума
-# ax_x_max = max(pressureHg) + 5/100 * (max(pressureHg) - min(pressureHg)) # на 5% длины всего графика вверх от максимума
-# ax_y_min = min(pressureADC) - 5/100 * (max(pressureADC) - min(pressureADC))
-# ax_y_max = max(pressureADC) + 5/100 * (max(pressureADC) - min(pressureADC))
-# ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
-#
-# # Добавляем подписи к осям:
-# ax.set_xlabel('Давление [мм.рт.ст.]')
-# ax.set_ylabel('Отсчеты АЦП')
-#
-# # заголовок графика
-# ax.set_title("Калибровочный график зависимости показаний АЦП от давления", loc = "center", wrap=True)
-#
-# # сетка
-# ax.minorticks_on()
-# # Определяем внешний вид линий основной сетки:
-# ax.grid(which='major',
-#         color = 'gray',  # цвет линий
-#         linewidth = 1, # толщина
-#         linestyle = '-'
-#         )
-# # Определяем внешний вид линий вспомогательной сетки:
-# ax.grid(which='minor',
-#         color = 'gray',  # цвет линий
-#         linewidth = 0.5, # толщина
-#         linestyle = '--'
-#         )
-#
-# #сохранение графика
-# fig.savefig("pressure-calibration.png")
+w = 8
+h = 5
+dpi = 400
+fig, ax = plt.subplots(figsize=(w,h), dpi=dpi)
+
+ax.scatter(pressureHg, pressureADC,
+           label="Измерения", c="blue", s=40, marker='*', alpha=1)
+
+ax.plot(pressureHg, pressureADC,
+        linestyle='-', linewidth=1.5, color='orange',
+        label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
+        )
+
+ax.legend(loc="upper left") # легенда
+
+# макс и мин занчения по осям
+ax_x_min = min(pressureHg) - 5/100 * (max(pressureHg) - min(pressureHg)) # на 5% длины всего графика вниз от минимума
+ax_x_max = max(pressureHg) + 5/100 * (max(pressureHg) - min(pressureHg)) # на 5% длины всего графика вверх от максимума
+ax_y_min = min(pressureADC) - 5/100 * (max(pressureADC) - min(pressureADC))
+ax_y_max = max(pressureADC) + 5/100 * (max(pressureADC) - min(pressureADC))
+ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
+
+# Добавляем подписи к осям:
+ax.set_xlabel('Давление [мм.рт.ст.]')
+ax.set_ylabel('Отсчеты АЦП')
+
+# заголовок графика
+ax.set_title("Калибровочный график зависимости показаний АЦП от давления", loc = "center", wrap=True)
+
+# сетка
+ax.minorticks_on()
+# Определяем внешний вид линий основной сетки:
+ax.grid(which='major',
+        color = 'gray',  # цвет линий
+        linewidth = 1, # толщина
+        linestyle = '-'
+        )
+# Определяем внешний вид линий вспомогательной сетки:
+ax.grid(which='minor',
+        color = 'gray',  # цвет линий
+        linewidth = 0.5, # толщина
+        linestyle = '--'
+        )
+
+#сохранение графика
+fig.savefig("pressure-calibration.png")
 # конец построения калибровочного графика
 
 
@@ -89,60 +89,60 @@ rest_samples_Hg = rest_samples_Hg[:rest_len//2]
 rest_time = rest_time[:rest_len//2]
 
 # начало построения графика давления до упражнений
-# w = 8
-# h = 5
-# dpi = 400
-# fig, ax = plt.subplots(figsize=(w, h), dpi=dpi)
-#
-# # маркеры далений
-# ax.scatter(rest_pressure_time, rest_pressure,
-#            marker='*', c="red", s=10, alpha=1, zorder=2)
-#
-# # подписи маркеров
-# ax.text(rest_pressure_time[0]*1.05, rest_pressure[0]*1.05, "Systole",
-#         color='black', fontsize=10)
-# ax.text(rest_pressure_time[1]*1.05, rest_pressure[1]*1.05, "Diastole",
-#         color='black', fontsize=10)
-#
-# ax.plot(rest_time, rest_samples_Hg,
-#         linestyle='-', linewidth=1.5, color='blue', zorder=1,
-#         label="Давление - {}/{} [мм.рт.ст.]".format(rest_pressure[0], rest_pressure[1])
-#         # label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
-#         )
-#
-# ax.legend(loc="upper right") # легенда
-#
-# # макс и мин занчения по осям
-# ax_x_min = min(rest_time) - 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вниз от минимума
-# ax_x_max = max(rest_time) + 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вверх от максимума
-# ax_y_min = min(rest_samples_Hg) - 5/100 * (max(rest_samples_Hg) - min(rest_samples_Hg))
-# ax_y_max = max(rest_samples_Hg) + 5/100 * (max(rest_samples_Hg) - min(rest_samples_Hg))
-# ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
-#
-# # Добавляем подписи к осям:
-# ax.set_xlabel('Время [с]')
-# ax.set_ylabel('Давление [мм.рт.ст.]')
-#
-# # заголовок графика
-# ax.set_title("Артериальное давление до физической нагрузки", loc = "center", wrap=True)
-#
-# # сетка
-# ax.minorticks_on()
-# # Определяем внешний вид линий основной сетки:
-# ax.grid(which='major',
-#         color = 'gray',  # цвет линий
-#         linewidth = 1, # толщина
-#         linestyle = '-'
-#         )
-# # Определяем внешний вид линий вспомогательной сетки:
-# ax.grid(which='minor',
-#         color = 'gray',  # цвет линий
-#         linewidth = 0.5, # толщина
-#         linestyle = '--'
-#         )
-#
-# #сохранение графика
-# fig.savefig("rest-pressure.png")
+w = 8
+h = 5
+dpi = 400
+fig, ax = plt.subplots(figsize=(w, h), dpi=dpi)
+
+# маркеры далений
+ax.scatter(rest_pressure_time, rest_pressure,
+           marker='*', c="red", s=10, alpha=1, zorder=2)
+
+# подписи маркеров
+ax.text(rest_pressure_time[0]*1.05, rest_pressure[0]*1.05, "Systole",
+        color='black', fontsize=10)
+ax.text(rest_pressure_time[1]*1.05, rest_pressure[1]*1.05, "Diastole",
+        color='black', fontsize=10)
+
+ax.plot(rest_time, rest_samples_Hg,
+        linestyle='-', linewidth=1.5, color='blue', zorder=1,
+        label="Давление - {}/{} [мм.рт.ст.]".format(rest_pressure[0], rest_pressure[1])
+        # label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
+        )
+
+ax.legend(loc="upper right") # легенда
+
+# макс и мин занчения по осям
+ax_x_min = min(rest_time) - 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вниз от минимума
+ax_x_max = max(rest_time) + 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вверх от максимума
+ax_y_min = min(rest_samples_Hg) - 5/100 * (max(rest_samples_Hg) - min(rest_samples_Hg))
+ax_y_max = max(rest_samples_Hg) + 5/100 * (max(rest_samples_Hg) - min(rest_samples_Hg))
+ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
+
+# Добавляем подписи к осям:
+ax.set_xlabel('Время [с]')
+ax.set_ylabel('Давление [мм.рт.ст.]')
+
+# заголовок графика
+ax.set_title("Артериальное давление до физической нагрузки", loc = "center", wrap=True)
+
+# сетка
+ax.minorticks_on()
+# Определяем внешний вид линий основной сетки:
+ax.grid(which='major',
+        color = 'gray',  # цвет линий
+        linewidth = 1, # толщина
+        linestyle = '-'
+        )
+# Определяем внешний вид линий вспомогательной сетки:
+ax.grid(which='minor',
+        color = 'gray',  # цвет линий
+        linewidth = 0.5, # толщина
+        linestyle = '--'
+        )
+
+#сохранение графика
+fig.savefig("rest-pressure.png")
 # конец построения графика давления до упражнений
 
 # график пульса
@@ -152,54 +152,54 @@ rest_pulse = [ rest_samples_Hg[i+step] - rest_samples_Hg[i] for i in range(0, le
 rest_time_pulse = [rest_time[i+step] for i in range(0, len(rest_samples_Hg)-step, step)]
 
 # начало построения графика пульса до упражнений
-# w = 8*2
-# h = 5
-# dpi = 400
-# fig, ax = plt.subplots(figsize=(w, h), dpi=dpi)
-#
-# ax.plot(rest_time_pulse, rest_pulse,
-#         linestyle='-', linewidth=1.5, color='blue',
-#         label="Пульс - {} [уд/мин]".format(rest_pulse_value)
-#         # label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
-#         )
-#
-# ax.legend(loc="upper right") # легенда
-#
-# # макс и мин занчения по осям
-# # ax_x_min = min(rest_time) - 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вниз от минимума
-# # ax_x_max = max(rest_time) + 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вверх от максимума
-# # ax_y_min = min(rest_pulse) - 5/100 * (max(rest_pulse) - min(rest_pulse))
-# # ax_y_max = max(rest_pulse) + 5/100 * (max(rest_pulse) - min(rest_pulse))
-# ax_x_min = 0
-# ax_x_max = 30
-# ax_y_min = -1.5
-# ax_y_max = 1.5
-# ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
-#
-# # Добавляем подписи к осям:
-# ax.set_xlabel('Время [с]')
-# ax.set_ylabel('Изменение давления в артерии [мм.рт.ст.]')
-#
-# # заголовок графика
-# ax.set_title("Пульс до физической нагрузки", loc = "center", wrap=True)
-#
-# # сетка
-# ax.minorticks_on()
-# # Определяем внешний вид линий основной сетки:
-# ax.grid(which='major',
-#         color = 'gray',  # цвет линий
-#         linewidth = 1, # толщина
-#         linestyle = '-'
-#         )
-# # Определяем внешний вид линий вспомогательной сетки:
-# ax.grid(which='minor',
-#         color = 'gray',  # цвет линий
-#         linewidth = 0.5, # толщина
-#         linestyle = '--'
-#         )
-#
-# #сохранение графика
-# fig.savefig("rest-pulse.png")
+w = 8*2
+h = 5
+dpi = 400
+fig, ax = plt.subplots(figsize=(w, h), dpi=dpi)
+
+ax.plot(rest_time_pulse, rest_pulse,
+        linestyle='-', linewidth=1.5, color='blue',
+        label="Пульс - {} [уд/мин]".format(rest_pulse_value)
+        # label="P = {:.3f} * N - {:.2f} [мм.рт.ст.]".format(p_ADC_to_Hg[0], abs(p_ADC_to_Hg[1])),
+        )
+
+ax.legend(loc="upper right") # легенда
+
+# макс и мин занчения по осям
+# ax_x_min = min(rest_time) - 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вниз от минимума
+# ax_x_max = max(rest_time) + 5/100 * (max(rest_time) - min(rest_time)) # на 5% длины всего графика вверх от максимума
+# ax_y_min = min(rest_pulse) - 5/100 * (max(rest_pulse) - min(rest_pulse))
+# ax_y_max = max(rest_pulse) + 5/100 * (max(rest_pulse) - min(rest_pulse))
+ax_x_min = 0
+ax_x_max = 30
+ax_y_min = -1.5
+ax_y_max = 1.5
+ax.axis( [ax_x_min, ax_x_max, ax_y_min, ax_y_max] )
+
+# Добавляем подписи к осям:
+ax.set_xlabel('Время [с]')
+ax.set_ylabel('Изменение давления в артерии [мм.рт.ст.]')
+
+# заголовок графика
+ax.set_title("Пульс до физической нагрузки", loc = "center", wrap=True)
+
+# сетка
+ax.minorticks_on()
+# Определяем внешний вид линий основной сетки:
+ax.grid(which='major',
+        color = 'gray',  # цвет линий
+        linewidth = 1, # толщина
+        linestyle = '-'
+        )
+# Определяем внешний вид линий вспомогательной сетки:
+ax.grid(which='minor',
+        color = 'gray',  # цвет линий
+        linewidth = 0.5, # толщина
+        linestyle = '--'
+        )
+
+#сохранение графика
+fig.savefig("rest-pulse.png")
 # конец построения графика пульса до упражнений
 
 
